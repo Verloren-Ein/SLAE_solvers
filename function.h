9f5d46f2fc0123439d15c8662948958e9383801b
Verloren-Ein <57700813+Verloren-Ein@users.noreply.github.com>
2019-11-13 14:39:33 +0700

    Create function.h

---
+++ function.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "common.h"
+
+complex<double> conjugate_prod(const vector<complex<double>> &a, const vector<complex<double>> &b);
+complex<double> dot_prod(const vector<complex<double>> &a, const vector<complex<double>> &b);
+double dot_prod_2(const vector<complex<double>> &a, const vector<complex<double>> &b);
+double dot_prod(const vector<complex<double>> &a);
+
